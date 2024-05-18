822b541fc89f4a9a0fbd99f5fea94722a48f6474
Dong-geon24 <rkdtjtneh@kyonggi.ac.kr>
2024-05-18 18:41:19 +0900

    [add] [update] +world file, cmake file edit - world install

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -15,20 +15,32 @@ def generate_launch_description():
 
     package_name = "my_robot"
     robot_file = "skidbot.urdf"
-    pkg_path = os.path.join(get_package_share_directory(package_name))
-    urdf_file = os.path.join(pkg_path,"description",robot_file)
+    world_file_name = "room_world"
 
-    pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')  
+    pkg_path = os.path.join(get_package_share_directory(package_name))
+    world_path = os.path.join(pkg_path, "worlds", world_file_name)
+    urdf_file = os.path.join(pkg_path, "description", robot_file)
+
+    pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')
+
+    # # Start Gazebo server
+    # start_gazebo_server_cmd = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
+    #     launch_arguments={'world': world_path}.items()
+    # )
+
     # Start Gazebo server
     start_gazebo_server_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
-        # launch_arguments={'world': world_path}.items()
+        launch_arguments={'world': world_path}.items()
     )
 
     # Start Gazebo client    
     start_gazebo_client_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py'))
     )
+    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    print(world_file_name,world_path)
 
     spawn_entity = Node(
         package='gazebo_ros',
@@ -40,6 +52,8 @@ def generate_launch_description():
         [
             start_gazebo_server_cmd,
             start_gazebo_client_cmd,
-            spawn_entity
+            spawn_entity,
+            
         ]
     )
+
